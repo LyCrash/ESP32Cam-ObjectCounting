5c997778a9a2f24fc1f4d7e556ed34c87d197428
LyCrash <kl_mouhoun@esi.dz>
2024-02-17 01:04:45 +0100

    new attempt: YOLOv7 - deepSort tracking > generic use

--- data/generate.py
+++ data/generate.py
@@ -1,14 +1,14 @@
 import requests
 import cv2
 
-# url = "https://www.pexels.com/download/video/12116094/?fps=29.97&h=720&w=1280"
-# response = requests.get(url, stream=True)
-# with open("video.mp4", "wb") as f:
-#     for chunk in response.iter_content(chunk_size=1024):
-#         f.write(chunk)
+url = "https://www.pexels.com/download/video/12116094/?fps=29.97&h=720&w=1280"
+response = requests.get(url, stream=True)
+with open("video.mp4", "wb") as f:
+    for chunk in response.iter_content(chunk_size=1024):
+        f.write(chunk)
 
 # Replace with the path to your downloaded video
-video_path = "Road_traffic.mp4"
+video_path = "video.mp4"
 
 # Open the video
 cap = cv2.VideoCapture(video_path)
@@ -25,9 +25,8 @@ duration = frame_count / fps
 cut_frame = int(frame_count / 4)  # Frame to cut the video at
 
 # Define the codec and create VideoWriter object
-fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-out = cv2.VideoWriter('short_Road_traffic.mp4', fourcc, fps,
-                      (int(cap.get(3)), int(cap.get(4))))
+fourcc = cv2.VideoWriter_fourcc(*'mp4v') 
+out = cv2.VideoWriter('short_video.mp4', fourcc, fps, (int(cap.get(3)), int(cap.get(4))))
 
 # Read and write frames until the cut point
 frame_num = 0
